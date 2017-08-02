5b96b966969a81aeca7239a6d8d2094eb0c4dece
l552177239 <552177239@qq.com>
2017-08-02 10:06:01 +0000

    file

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # 上传文件
 
 初始化项目 upload-test
+
+### 选择文件
+
+利用`input`的 file 类型进行文件的选择
+
+```
+handleChange = () => {
+  console.log(1)
+}
+
+<input type='file' className='file-input' onChange={this.handleChange} />
+```
+
+如果选择成功，控制台输出`1`
--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 
 class App extends React.Component{
   handleChange = () => {
-
+    console.log(1)
   }
   render(){
     return(
       <div className="App">
-        App
+        <input type='file' className='file-input' onChange={this.handleChange} />
       </div>
     )
   }
