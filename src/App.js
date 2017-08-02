5316b3eed5f4c5110ef25eca31e08d44b855a2c5
l552177239 <552177239@qq.com>
2017-08-02 09:59:13 +0000

    create

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component{
+  handleChange = () => {
+
+  }
+  render(){
+    return(
+      <div className="App">
+        App
+      </div>
+    )
+  }
+}
+
+export default App
